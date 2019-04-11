6c14a0b473404ad371fa4cb8c3613c916d0a60da
ashique <ashique12009@gmail.com>
2019-04-12 01:07:07 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+io.on('connection', function(socket){
+    socket.on('chat', function(msg){
+        io.emit('chat', msg);
+    });
+});
+
+io.on('disconnect', function(){
+    console.log('User is disconnected!');
+});
+
+http.listen(3000, function(){
+    console.log('Listening on port 3000');
+});
